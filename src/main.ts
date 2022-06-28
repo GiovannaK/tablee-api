0fd65a90ad1e8498072ca49ecc0f67c7032bbf6d
Giovanna Cunha <giovannakelli@gmail.com>
2022-06-28 00:09:22 -0300

    feat: add raw body middleware to consume webhook

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import rawBodyMiddleware from './middlewares/rawBody.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(rawBodyMiddleware);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(parseInt(process.env.PORT) || 4000);
 }
